d03cb28b5ff4523291045bdd603271d271bc8d9a
honggeolgim <64815876+honggeolgim@users.noreply.github.com>
2020-05-05 09:14:38 +0900

    테스트 첫 컷밋

---
+++ Hello world/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, {0}", args[0]);
+
+        }
+    }
+}
